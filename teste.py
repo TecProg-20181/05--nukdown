3e8bba4b3b468242860f38143bb40bb90d8a2731
Nukdown <belchior.emanoel@gmail.com>
2018-06-22 21:00:40 -0300

    make test for subprocess output method

--- teste.py
+++ teste.py
@@ -1,4 +1,5 @@
 import unittest
+import subprocess
 
 from diskspace import diskspace
 
@@ -9,3 +10,9 @@ class TestClass(unittest.TestCase):
 
     def tear_down(self):
         pass
+
+    def test_subprocess_check(self):
+        string = 'du -d 1'
+        rawstring = diskspace.subprocess_check_output(string)
+        self.assertEqual(subprocess.check_output(string.strip().split(' ')),
+                        rawstring)
